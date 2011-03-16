2435f6b2884f11b1d562c18f360264d3eb455f47
Jamie McClelland <jm@mayfirst.org>
2011-03-15 21:14:24 -0400

    provide pgsql support

---
+++ manifests/pgsql.pp
@@ -0,0 +1,29 @@
+# Safe PGSQL dumps, as part of a backupninja run.
+#
+# Valid attributes for this type are:
+#
+#   order: The prefix to give to the handler config filename, to set
+#      order in which the actions are executed during the backup run.
+#
+#   ensure: Allows you to delete an entry if you don't want it any more
+#      (but be sure to keep the configdir, name, and order the same, so
+#      that we can find the correct file to remove).
+#
+#   backupdir, compress, configfile: As defined in the
+#   backupninja documentation, with the caveat that hotcopy, sqldump,
+#   and compress take true/false rather than yes/no.
+# 
+define backupninja::pgsql(
+  $order = 10, $ensure = present, $databases = 'all', $backupdir = "/var/backup/postgres", $compress = true, $vsname = false)
+{
+  
+  include backupninja::client::defaults
+  file { "${backupninja::client::defaults::configdir}/${order}_${name}.pgsql":
+    ensure => $ensure,
+    content => template('backupninja/pgsql.conf.erb'),
+    owner => root,
+    group => root,
+    mode => 0600,
+    require => File["${backupninja::client::defaults::configdir}"]
+  }
+}
